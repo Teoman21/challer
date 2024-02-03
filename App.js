642e74aba2f7186cb5e2ae9884fe022c0b4d09fa
Teoman21 <teomankaman@gmail.com>
2024-02-03 23:42:40 +0300

    setup structure and update package

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
 
-export default function App() {
+import Messages from './components/Messages.js';
+import Home from './components/Home.js';
+import More from './components/More.js';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Messages" component={Messages} />
+          <Stack.Screen name="More" component={More} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>CHALLER</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +42,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  wrapper: {
+    flex: 1,
+
+  },
 });
+
+export default App;
