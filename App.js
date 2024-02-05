d7fc1d851e631ef331ac99968a6db0c0576665a2
Teoman21 <teomankaman@gmail.com>
2024-02-05 18:22:42 +0300

    Added components to their folder and add some icons to navbar

--- App.js
+++ App.js
@@ -1,50 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 import React from "react";
 
-import Messages from './components/Messages.js';
-import Home from './components/Home.js';
-import More from './components/More.js';
+import Messages from './components/Messages/Messages.js';
+import More from './components/More/More.js';
+import Home from './components/Home/Home.js';
 
-const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <NavigationContainer>
-        <Tab.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Messages" component={Messages} />
-          <Stack.Screen name="More" component={More} />
+        <Tab.Navigator
+          initialRouteName="Swipe"
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === 'Home') {
+                iconName = focused ? 'home' : 'home-outline';
+              } else if (route.name === 'Messages') {
+                iconName = focused ? 'chatbox' : 'chatbox-outline';
+              } else if (route.name === 'More') {
+                iconName = focused ? 'apps' : 'apps-outline';
+              } 
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'white',
+            tabBarInactiveTintColor: 'gray',
+            tabBarStyle: {
+              backgroundColor: '#1A2275', // Set the background color
+              borderTopColor: 'gray',
+            },
+            tabBarItemStyle: {
+              borderLeftWidth: 1, // Border between each item
+              borderLeftColor: 'gray',
+            },
+            headerShown: false,
+          })}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Messages" component={Messages} />
+          <Tab.Screen name="More" component={More} />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
 }
 
-const HomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>CHALLER</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   wrapper: {
     flex: 1,
-
+    backgroundColor: '#1A2275',
   },
 });
 
